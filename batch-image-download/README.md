14bfa5913e2d340d04422cff14a4b8ada83cd244
Tohr01 <carl@kratzundraabe.de>
2025-09-10 21:45:24 +0200

    core: base functionality

---
+++ batch-image-download/README.md
@@ -0,0 +1,32 @@
+# Batch image downloader
+A small utility script to batch download images from a list of urls.
+
+## 📝 Requirements
+- Python3 (pref. 3.8+)
+> [!NOTE]
+> This script does not depend on other Python packages and only uses the built in packages
+
+## 🛠️ Installation
+1. Download the `download.py` script
+<br />
+You might be getting an error like this:
+```urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1108)```
+To resolve this run the `Install Certificates.command` on macOS located in /Applications/Python 3.X/
+
+## 🚀 Usage
+```
+>> python3 download.py --help
+usage: Batch image downloader [-h] [-u URLS] [-i IMAGES] [-c] [-t TIMEOUT]
+
+A small utility script to batch download images from a list of urls.
+
+options:
+  -h, --help            show this help message and exit
+  -u URLS, --urls URLS  Path to the text (txt) file containing the image urls. Default is urls.txt in the current dir.
+  -i IMAGES, --images IMAGES
+                        Path to directory where to download the images into. Will create the dir if it does not exist. Default will create a images
+                        directory in the current dir.
+  -c, --clear           When this flag is set the script will clear the contents of the images directory.
+  -t TIMEOUT, --timeout TIMEOUT
+                        Timeout between download requests in milliseconds. Default is 0.
+```
---
+++ batch-image-download/download.py
@@ -0,0 +1,76 @@
+from mimetypes import guess_extension
+from pathlib import Path
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from re import compile
+from argparse import ArgumentParser
+from time import sleep
+
+argparser = ArgumentParser(prog='Batch image downloader', description='A small utility script to batch download images from a list of urls.')
+argparser.add_argument('-u', '--urls', help='Path to the text (txt) file containing the image urls. Default is urls.txt in the current dir.', default='./urls.txt', type=str)
+argparser.add_argument('-i', '--images', help='Path to directory where to download the images into. Will create the dir if it does not exist. Default will create a images directory in the current dir.', default='./images', type=str)
+argparser.add_argument('-c', '--clear', help='When this flag is set the script will clear the contents of the images directory.', action='store_true')
+argparser.add_argument('-t', '--timeout', help="Timeout between download requests in milliseconds. Default is 0.", default=0, type=int)
+args = argparser.parse_args()
+
+CLEAR_OUTPUT_DIR = args.clear
+URLS_FILE_PATH = Path(args.urls)
+IMAGES_DIR_PATH = Path(args.images)
+DOWNLOAD_TIMEOUT_MS = args.timeout 
+URL_RGX = compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+
+IMAGES_DIR_PATH.mkdir(exist_ok=True)
+if not IMAGES_DIR_PATH.is_dir():
+    raise NotADirectoryError(f'❌ {str(IMAGES_DIR_PATH)} is not a directory') 
+
+if CLEAR_OUTPUT_DIR:
+    for file in IMAGES_DIR_PATH.iterdir():
+        file.unlink()
+
+if not URLS_FILE_PATH.exists() and URLS_FILE_PATH.is_file():
+    raise FileNotFoundError('❌ File "urls.txt" was not found')
+
+# Get links from links.txt file
+with open(URLS_FILE_PATH, 'r') as urls_h:
+    lines = urls_h.read().split('\n')
+    urls = [*filter(lambda line: URL_RGX.match(line), lines)]
+
+print(f'🚀 Starting download of {len(urls)} urls')
+
+failed = 0
+for idx, url in enumerate(urls):
+    resp = urlopen(url)
+    stat_code = resp.getcode()
+    if stat_code != 200:
+        failed += 1
+        print(f'❌ Failed to download image. Server returned status code {stat_code}. Url: {url}')
+        continue
+
+    ext = None
+
+    content_type_header = resp.headers.get('Content-Type')
+    if content_type_header and content_type_header.startswith('image/'):
+        # Guess extention from mime type
+        ext = guess_extension(content_type_header)
+        # We go with jpg instead of jpe
+        if ext == '.jpe':
+            ext = '.jpg'
+    else:
+        # Determine from url
+        parsed_url = urlparse(url)
+        split_path = parsed_url.path.split('.')
+        if len(split_path) == 1:
+            failed += 1
+            print(f'❌ Could not determine extention for image url: {url}')
+            continue
+        ext = '.' + split_path[-1]
+
+    filename = f'{idx + 1}{ext}'
+    with open(IMAGES_DIR_PATH / filename, 'wb') as f:
+        f.write(resp.read())
+    print(f'✅ [{idx+1}/{len(urls)}] Successfully downloaded image from url: {url}')
+
+    # Wait for next iteration
+    if idx < len(urls) - 1: sleep(DOWNLOAD_TIMEOUT_MS / 1000)
+
+print(f'🏁 Successfully downloaded {len(urls) - failed}/{len(urls)} images')
